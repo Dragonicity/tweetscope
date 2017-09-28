5ea8ad748bc530530a9a8f3185b77fdea42a8538
Mark-Nelson <mark.nelson@dragonicity.com>
2017-09-28 17:58:17 +0800

    Add Twitter integration

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -1,2 +1,17 @@
 class Keyword < ApplicationRecord
+
+  def grab_tweets
+    client = Twitter::REST::Client.new do |config|
+      config.consumer_key        = "I6ujrVH2pyYDD9DcL2pik65Sh"
+      config.consumer_secret     = "WenT1lRXyw7AGU2ERXWmxbvDRwLa7irvpSOV8lYVKrw3nj8d9o"
+      config.access_token        = "1274583877-EArLKTY687X13XwKjcQTzYhhBKjxpAEi66mwjGK"
+      config.access_token_secret = "UDX3YxDGL7HIqcylgK7oTalf7dLyaOqLTYBdOhXtoKJrU"
+    end
+
+    client.search(self.word, :result_type => "recent").take(3).collect do |tweet|
+      "#{tweet.user.screen_name}: #{tweet.text}"
+    end
+
+  end
+
 end
