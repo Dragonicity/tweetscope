036f16d259e8dda9ce8b4364089de811088ee0b9
Mark-Nelson <mark.nelson@dragonicity.com>
2017-09-29 12:59:33 +0800

    Added scheduler

--- app/models/keyword.rb
+++ app/models/keyword.rb
@@ -9,7 +9,7 @@ class Keyword < ApplicationRecord
       config.access_token_secret = "UDX3YxDGL7HIqcylgK7oTalf7dLyaOqLTYBdOhXtoKJrU"
     end
 
-    client.search(self.word, :result_type => "recent").take(10).collect do |tweet|
+    client.search(self.word, :result_type => "recent").take(100).collect do |tweet|
       new_tweet = Tweet.new
       
       new_tweet.tweet_id          = tweet.id.to_s
@@ -18,11 +18,18 @@ class Keyword < ApplicationRecord
       new_tweet.user_uid          = tweet.user.id
       new_tweet.user_name         = tweet.user.name
       new_tweet.user_screen_name  = tweet.user.screen_name
-      new_tweet.user_image_url    = tweet.user.profile_image_url
+      new_tweet.user_image_url    = tweet.user.profile_image_url.to_s
       new_tweet.keyword           = self
 
       new_tweet.save
 
+    end
+
+  end
+
+  def self.grab_all_tweets
+    Keyword.all.each do |keyword|
+      keyword.grab_tweets
     end
 
   end
